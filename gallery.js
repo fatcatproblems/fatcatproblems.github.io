6698cba8a4e37f001eb82d5672781cb94d91e351
fatcatproblems <fatcatproblemsbg@gmail.com>
2026-02-18 10:45:31 +0200

    mobile fix

--- gallery.js
+++ gallery.js
@@ -1,41 +1,41 @@
 const allImages = [
-	{ name: '2021.05.24 Punx Picnic 9.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2021.05.24 Punx Picnic 9.jpg', category: 'concerts', title: 'Live in Sofia' },
     { name: '2021.12.11 Pizzza@Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
     { name: '2022.02.27 Хрибогон @ Строежа.jpg', category: 'concerts', title: 'Live in Sofia' },
     { name: '2022.05.04 Азнепия @ Строежа.jpg', category: 'concerts', title: 'Live in Sofia' },
     { name: '2022.05.19 @ Безкрай.jpg', category: 'concerts', title: 'Live in Sofia' },
-    { name: '2022.05.28 Рядка Порода @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },	
-	{ name: '2022.08.04 @BEAT.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2022.09.21 Superhiks @ BEAT.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2022.11.11 @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.01.14 @ Черепите.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.04.08 oXo @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.04.28 KotTakoa @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.06.10 ToyLetters @ Mixtape.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.08.27 БългарСка @ Grindhouse.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.09.29 FlyTheFlag @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.11.11 RaduRD @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2023.12.08 ToyLetters @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.01.13 НащеХора @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.02.08 ToyLetters @ ThinRedLine.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.05.31 CheeseHeads @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.10.18 RD @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.10.28 The Toasters @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.11.30 Adams.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.12.20 @ Pit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2024.12.28 @ Shumen.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2025.02.25 @ Carve.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2025.04.26 @ Pit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2025.10.25 Punky Blues.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: '2025.12.05 Зимен Дъжд, НаштеХора @ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
-	{ name: 'Kolyo.jpg', category: 'members', title: 'Live in Sofia' },
-	{ name: 'Murphy.jpg', category: 'members', title: 'Live in Sofia' },
-	{ name: 'Emo.jpg', category: 'members', title: 'Live in Sofia' },
-	{ name: 'Niki.jpg', category: 'members', title: 'Live in Sofia' },
-	{ name: 'Mitko.jpg', category: 'members', title: 'Live in Sofia' },
-	{ name: 'Majorkata.jpg', category: 'members', title: 'Live in Sofia' },
+    { name: '2022.05.28 Рядка Порода @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },   
+    { name: '2022.08.04 @BEAT.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2022.09.21 Superhiks @ BEAT.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2022.11.11 @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.01.14 @ Черепите.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.04.08 oXo @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.04.28 KotTakoa @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.06.10 ToyLetters @ Mixtape.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.08.27 БългарСка @ Grindhouse.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.09.29 FlyTheFlag @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.11.11 RaduRD @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2023.12.08 ToyLetters @ Maze.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.01.13 НащеХора @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.02.08 ToyLetters @ ThinRedLine.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.05.31 CheeseHeads @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.10.18 RD @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.10.28 The Toasters @ ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.11.30 Adams.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.12.20 @ Pit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2024.12.28 @ Shumen.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2025.02.25 @ Carve.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2025.04.26 @ Pit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2025.10.25 Punky Blues.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2025.12.05 Зимен Дъжд, НаштеХора @ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: 'Kolyo.jpg', category: 'members', title: 'Live in Sofia' },
+    { name: 'Murphy.jpg', category: 'members', title: 'Live in Sofia' },
+    { name: 'Emo.jpg', category: 'members', title: 'Live in Sofia' },
+    { name: 'Niki.jpg', category: 'members', title: 'Live in Sofia' },
+    { name: 'Mitko.jpg', category: 'members', title: 'Live in Sofia' },
+    { name: 'Majorkata.jpg', category: 'members', title: 'Live in Sofia' },
     { name: 'Asen.jpg', category: 'members', title: 'Live in Sofia' },
-    { name: 'Rado.jpg', category: 'members', title: 'Live in Sofia' }	
+    { name: 'Rado.jpg', category: 'members', title: 'Live in Sofia' }   
 ];
 
 let currentFilteredList = [];
@@ -46,14 +46,12 @@ const viewer = document.getElementById('fullSizeViewer');
 const fullImg = document.getElementById('fullSizeImg');
 const caption = document.getElementById('fullSizeCaption');
 
-// Помощна функция за изчистване на името от разширението .jpg
 function formatName(fileName) {
-    return fileName.replace(/\.[^/.]+$/, ""); // Премахва всичко след последната точка
+    return fileName.replace(/\.[^/.]+$/, ""); 
 }
 
 function renderGallery(filter = 'all') {
     grid.innerHTML = ''; 
-    
     if (filter === 'all') {
         currentFilteredList = [...allImages];
     } else {
@@ -63,9 +61,8 @@ function renderGallery(filter = 'all') {
     currentFilteredList.forEach((img, index) => {
         const item = document.createElement('div');
         item.className = 'photo-item';
-        
         const imagePath = `img/gallery/${img.name}`;
-        const displayName = formatName(img.name); // Използваме името на файла за заглавие
+        const displayName = formatName(img.name);
         
         item.innerHTML = `
             <img src="${imagePath}" alt="${displayName}" loading="lazy">
@@ -104,7 +101,7 @@ function changeImage(step) {
 function updateViewer() {
     const data = currentFilteredList[currentIndex];
     fullImg.src = `img/gallery/${data.name}`;
-    caption.innerText = formatName(data.name); // Показваме името и в големия изглед
+    caption.innerText = formatName(data.name);
 }
 
 // ЕВЕНТИ
@@ -124,5 +121,29 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
-// СТАРТИРАНЕ - Сега по подразбиране зарежда 'concerts'
+// --- SWIPE LOGIC ЗА МОБИЛНИ УСТРОЙСТВА ---
+let touchstartX = 0;
+let touchendX = 0;
+
+viewer.addEventListener('touchstart', e => {
+    touchstartX = e.changedTouches[0].screenX;
+}, {passive: true});
+
+viewer.addEventListener('touchend', e => {
+    touchendX = e.changedTouches[0].screenX;
+    handleSwipe();
+}, {passive: true});
+
+function handleSwipe() {
+    const swipeDistance = touchendX - touchstartX;
+    // Ако е плъзнато надясно (повече от 50px) -> предишна снимка
+    if (swipeDistance > 50) {
+        changeImage(-1);
+    }
+    // Ако е плъзнато наляво (повече от 50px) -> следваща снимка
+    else if (swipeDistance < -50) {
+        changeImage(1);
+    }
+}
+
 renderGallery('concerts');
