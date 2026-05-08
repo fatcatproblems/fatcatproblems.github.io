8843d25a6e1a136e82f6138fc4d22bc2cae36d0e
fatcatproblems <fatcatproblemsbg@gmail.com>
2026-05-08 12:01:29 +0300

    Voi gig

--- gallery.js
+++ gallery.js
@@ -30,6 +30,7 @@ const allImages = [
     { name: '2025.12.05 Зимен Дъжд, НаштеХора @ThePit.jpg', category: 'concerts', title: 'Live in Sofia' },
     { name: '2026.02.21 Toy Letters @ Carve.jpg', category: 'concerts', title: 'Live in Sofia' },
     { name: '2026.04.04 Mizeriq, MZP,CEME @ Cherepite.jpg', category: 'concerts', title: 'Live in Sofia' },
+    { name: '2026.05.16 Вой!@Carve.jpg', category: 'concerts', title: 'Live in Sofia' },
     
     // ЧЛЕНОВЕ С БИОГРАФИИ
     { name: 'Kolyo.jpg', category: 'members', title: 'Кольо', bio: 'Кольо е бас и глас в тази формация.' },
